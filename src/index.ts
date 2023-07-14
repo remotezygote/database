5f9eeb289a667906c14296f45d1f62aa3a6fbef3
John Bragg <john@getarch.com>
2023-07-14 09:50:16 -0700

    better listen behavior with errors allowed to raise

--- src/index.ts
+++ src/index.ts
@@ -29,32 +29,35 @@ export const queryWithCallback = async (text: string, params: any[] = [], callba
 
 export const listen = async (queue: string, onMessage: Function, exclusive: boolean = true) => {
 	const lockName = `('x'||substr(md5('listen-${queue}'),1,16))::bit(64)::bigint`
-	try {
-		const client = await pool.connect()
-		if (exclusive) {
-			client.query(`SELECT pg_advisory_lock(${lockName})`)
-		}
-		client.query(`LISTEN ${queue}`)
-		client.on('notification', ({ channel, payload }) => {
-			if (channel === queue) {
-				onMessage(JSON.parse(payload))
-			}
-		})
-		const stopper = () => {
-			client.query(`UNLISTEN ${queue}`)
-			if (exclusive) {
-				client.query(`SELECT pg_advisory_unlock(${lockName})`)
-			}
-			client.release()
-		}
-		process.on('SIGTERM', stopper)
-		client.on('error', e => {
-			console.error(e)
-			stopper()
-			process.exit(1)
-		})
-		return stopper
-	} catch (e) {
-		console.error(e)
+	const client = await pool.connect()
+	if (exclusive) {
+		await client.query(`SELECT pg_advisory_lock(${lockName})`)
 	}
+	console.log(`Listening to ${queue}...`)
+	await client.query(`LISTEN ${queue}`)
+	client.on('notification', ({ channel, payload }) => {
+		if (channel === queue) {
+			onMessage(JSON.parse(payload))
+		}
+	})
+	const stopper = async () => {
+		await client.query(`UNLISTEN ${queue}`)
+		if (exclusive) {
+			await client.query(`SELECT pg_advisory_unlock(${lockName})`)
+		}
+		client.release()
+	}
+	process.on('SIGTERM', stopper)
+	client.on('error', e => {
+		console.error('Database client error')
+		console.error(e)
+		try {
+			stopper()
+		} catch (err) {
+			console.error(err)
+		} finally {
+			process.exit(1)
+		}
+	})
+	return stopper
 }
